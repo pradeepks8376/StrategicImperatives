528cdb0d5407571ddeb9754523595de19a01c62d
pradeepks <kumarks.pradeep@gmail.com>
2024-09-06 21:26:03 +0100

    Created Exception Error Handler

---
+++ JavaExercise/src/main/java/uk/co/imperatives/exercise/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package uk.co.imperatives.exercise.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
+
